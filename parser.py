45893477db58dc0ec4619098558bc6bd992904e6
Coding <alextoulaki@gmail.com>
2018-05-23 10:43:52 +0300

    Add files via upload

--- parser.py
+++ parser.py
@@ -8,10 +8,15 @@ class ParseError(Exception):
 class RunError(Exception):
     pass
 
-
 class MyParser():
 
+    def __init__(self):
+        self.st = {}
+
     def create_scanner(self, fp):
+        letter = plex.Range("azAZ")
+        digit = plex.Range("09")
+
         letter = plex.Range("azAZ")
         digit = plex.Range("09")
 
@@ -22,7 +27,7 @@ class MyParser():
         identifier = letter + plex.Rep(letter | digit)
         assign = plex.Str("=")
         space = plex.Rep1(plex.Any(" \n\t"))
-        par = plex.Str("(", ")")
+        par = plex.Any("()")
 
         lexicon = plex.Lexicon([
             (kprint, plex.TEXT),
@@ -34,7 +39,7 @@ class MyParser():
             (par, plex.TEXT),
             (space, plex.IGNORE)
         ])
-
+        
         self.scanner = plex.Scanner(lexicon, fp)
         self.la, self.val = self.next_token()
 
@@ -52,7 +57,7 @@ class MyParser():
         self.stmt_list()
 
     def stmt_list(self):
-        if self.la == "IDENTIFIER" or self.la == "print":
+        if self.la == "IDENTIFIER" or self.la == "print": 
             self.stmt()
             self.stmt_list()
         elif self.la is None:
@@ -61,8 +66,8 @@ class MyParser():
             raise ParseError("Expected: identifier or print")
 
     def stmt(self):
-        # print("stmt: ", self.la)
         if self.la == "IDENTIFIER":
+            varname = self.val
             self.match("IDENTIFIER")
             self.match("=")
             self.expr()
@@ -71,65 +76,78 @@ class MyParser():
             self.expr()
         else:
             raise ParseError("Expected: identifier or print")
+
     def expr(self):
-        if self.la == "(" or self.la == "True" or self.la == "not" or self.la  == "False" or self.la == "IDENTIFIER":
+        if self.la in  self.la == "(" or self.la == "IDENTIFIER" or self.la == "not" or self.la == "True" or self.la == "False":
             self.term()
             self.term_tail()
         else:
-            raise ParseError("Exppected: '(' or identifier or boolean value")
+            raise ParseError("Expected: '(' or identifier or not or boolean value")
 
     def term_tail(self):
         if self.la == "or":
+           # print("term-tail", self.la)
             self.orop()
             self.term()
             self.term_tail()
             return
-        elif self.la == "print" or self.la is None or self.la == "IDENTIFIER" or self.la == "print":
+        elif self.la == "IDENTIFIER" or self.la == "print" or self.la == ")" or self.la is None: 
             return
         else:
-            raise ParseError('Expected print or None or IDENTIFIER ')
+             raise ParseError('Expected or print or None or identifier or )  ')
 
     def term(self):
-        if self.la == "False" or self.la == "True" or self.la == "(" or self.la == "not":
-            self.factor()
+        if self.la == "(" or self.la == "not" or self.la == "IDENTIFIER" or self.la == "True" or self.la == "False": 
+            self.negative_factor()
             self.factor_tail()
             return
         else:
-             raise ParseError("Expected: '(' or identifier or boolean value")
+            raise ParseError("Expected: '(' or  not  or identifier or boolean value")
 
     def factor_tail(self):
         if self.la == "and":
             self.andop()
-            self.factor()
+            #f = self.factor()
+            self.negative_factor()
             self.factor_tail()
             return
-        elif self.la == "or" or  self.la == ")" or self.la == "IDENTIFIER" or self.la == "print" or self.la is None:
+        elif self.la == "or" or self.la == "print" or self.la =="IDENTIFIER" or self.la == ")" or self.la is None:   
             return
         else:
-            raise ParseError("Expected: 'and'")
+            raise ParseError("Expected: and or print  identifier )")
+
+    def negative_factor(self):
+        negative = False
+        if self.la == 'not':
+            negative = True
+            self.match("not")
+        return self.factor()
 
     def factor(self):
-        if self.la == "not":
-            self.match("not") 
         if self.la == '(':
             self.match('(')
             self.expr()
             self.match(')')
-            return
-        elif self.la =="(" or self.la == "IDENTIFIER" or self.la == "True" or self.la =="False":
+        elif self.la == "IDENTIFIER":
             self.match(self.la)
+        elif self.la == "True" or self.la == "False":
+            token = self.la
+            self.match(token)
+            return 
         else:
             raise ParseError("Expected: notop id,  notop(expr), notop values, (expr), id, notop boolean")
 
     def orop(self):
         if self.la == "or":
             self.match("or")
+            return "or"
         else:
             raise ParseError("Expected: 'or'")
 
     def andop(self):
         if self.la == "and":
             self.match("and")
+            return "and"
         else:
             raise ParseError("Expected: 'and'")
 
@@ -139,5 +157,5 @@ fp = open("data.txt", "r")
 try:
     p.parse(fp)
 except ParseError as perr:
-    print(perr)
+    print("perr",  perr)
 fp.close()
