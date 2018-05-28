ee5d4b5304de3b3b2b421c711eaa842f9ee3b824
Coding <alextoulaki@gmail.com>
2018-05-28 13:39:19 +0300

    Add files via upload

--- parser.py
+++ parser.py
@@ -1,161 +1,235 @@
+#!/usr/bin/env python3
+# -*- coding: utf_8 -*-
+
+"""
+
+Session  -> Facts Question | ( Session ) Session
+Facts    -> Fact Facts | ε
+Fact     -> ! string
+Question -> ? string
+
+FIRST sets
+----------
+Session:  ( ? !
+Facts:    ε !
+Fact:     !
+Question: ?
+
+FOLLOW sets
+-----------
+Session:  # )
+Facts:    ?
+Fact:     ! ?
+Question: # )  
+"""
 import plex
 
-
 class ParseError(Exception):
-    pass
-
-
+	""" A user defined exception class, to describe parse errors. """
+	pass
 class RunError(Exception):
-    pass
+	pass
 
-class MyParser():
+def join_list(lst, blackmail_lst = []):
+	return ' '.join(str(e) for e in lst if str(e) not in blackmail_lst )
 
-    def __init__(self):
-        self.st = {}
 
-    def create_scanner(self, fp):
-        letter = plex.Range("azAZ")
-        digit = plex.Range("09")
+class MyParser:
+	""" A class encapsulating all parsing functionality
+	for a particular grammar. """
 
-        letter = plex.Range("azAZ")
-        digit = plex.Range("09")
+	def __init__(self, debug = True):
+		self.debug = debug
 
-        ops = plex.Str("not", "and", "or")
-        kprint = plex.Str("print")
-        true = plex.NoCase(plex.Str("true", "t", "1"))
-        false = plex.NoCase(plex.Str("false", "f", "0"))
-        identifier = letter + plex.Rep(letter | digit)
-        assign = plex.Str("=")
-        space = plex.Rep1(plex.Any(" \n\t"))
-        par = plex.Any("()")
+	def debug_message(self, caller):
+		if self.debug:
+			print('--------------------------------')
+			print('DEBUG MESSAGE : ')
+			print('CALLER : ', caller)
+			print('LOOK_AHEAD : ', self.la)
 
-        lexicon = plex.Lexicon([
-            (kprint, plex.TEXT),
-            (ops, plex.TEXT),
-            (true, "True"),
-            (false, "False"),
-            (identifier, "IDENTIFIER"),
+	
+	def create_scanner(self,fp):
+		""" Creates a plex scanner for a particular grammar 
+		to operate on file object fp. """
+
+		letter = plex.Range("azAZ")
+		digit = plex.Range("09")
+
+		ops = plex.Str("not", "and", "or")
+		kprint = plex.Str("print")
+		true = plex.NoCase(plex.Str("true", "t", "1"))
+		false = plex.NoCase(plex.Str("false", "f", "0"))
+		identifier = letter + plex.Rep(letter | digit)
+		assign = plex.Str("=")
+		space = plex.Rep1(plex.Any(" \n\t"))
+		par = plex.Any("()")
+
+		lexicon = plex.Lexicon([
+			(kprint, "PRINT"),
+			(ops, plex.TEXT),
+            (true, "TRUE"),
+            (false, "FALSE"),
+            (identifier, "ID"),
             (assign, "="),
             (par, plex.TEXT),
             (space, plex.IGNORE)
         ])
         
-        self.scanner = plex.Scanner(lexicon, fp)
-        self.la, self.val = self.next_token()
-
-    def next_token(self):
-        return self.scanner.read()
-
-    def match(self, token):
-        if self.la == token:
-            self.la, self.val = self.next_token()
-        else:
-            raise ParseError("Expected: ", self.la)
-
-    def parse(self, fp):
-        self.create_scanner(fp)
-        self.stmt_list()
-
-    def stmt_list(self):
-        if self.la == "IDENTIFIER" or self.la == "print": 
-            self.stmt()
-            self.stmt_list()
-        elif self.la is None:
-            return
-        else:
-            raise ParseError("Expected: identifier or print")
-
-    def stmt(self):
-        if self.la == "IDENTIFIER":
-            varname = self.val
-            self.match("IDENTIFIER")
-            self.match("=")
-            self.expr()
-        elif self.la == "print":
-            self.match("print")
-            self.expr()
-        else:
-            raise ParseError("Expected: identifier or print")
-
-    def expr(self):
-        if self.la in  self.la == "(" or self.la == "IDENTIFIER" or self.la == "not" or self.la == "True" or self.la == "False":
-            self.term()
-            self.term_tail()
-        else:
-            raise ParseError("Expected: '(' or identifier or not or boolean value")
-
-    def term_tail(self):
-        if self.la == "or":
-           # print("term-tail", self.la)
-            self.orop()
-            self.term()
-            self.term_tail()
-            return
-        elif self.la == "IDENTIFIER" or self.la == "print" or self.la == ")" or self.la is None: 
-            return
-        else:
-             raise ParseError('Expected or print or None or identifier or )  ')
-
-    def term(self):
-        if self.la == "(" or self.la == "not" or self.la == "IDENTIFIER" or self.la == "True" or self.la == "False": 
-            self.negative_factor()
-            self.factor_tail()
-            return
-        else:
-            raise ParseError("Expected: '(' or  not  or identifier or boolean value")
-
-    def factor_tail(self):
-        if self.la == "and":
-            self.andop()
-            #f = self.factor()
-            self.negative_factor()
-            self.factor_tail()
-            return
-        elif self.la == "or" or self.la == "print" or self.la =="IDENTIFIER" or self.la == ")" or self.la is None:   
-            return
-        else:
-            raise ParseError("Expected: and or print  identifier )")
-
-    def negative_factor(self):
-        negative = False
-        if self.la == 'not':
-            negative = True
-            self.match("not")
-        return self.factor()
-
-    def factor(self):
-        if self.la == '(':
-            self.match('(')
-            self.expr()
-            self.match(')')
-        elif self.la == "IDENTIFIER":
-            self.match(self.la)
-        elif self.la == "True" or self.la == "False":
-            token = self.la
-            self.match(token)
-            return 
-        else:
-            raise ParseError("Expected: notop id,  notop(expr), notop values, (expr), id, notop boolean")
-
-    def orop(self):
-        if self.la == "or":
-            self.match("or")
-            return "or"
-        else:
-            raise ParseError("Expected: 'or'")
-
-    def andop(self):
-        if self.la == "and":
-            self.match("and")
-            return "and"
-        else:
-            raise ParseError("Expected: 'and'")
+		self.scanner = plex.Scanner(lexicon, fp)
+		self.la, self.val = self.next_token()
 
 
-p = MyParser()
-fp = open("data.txt", "r")
-try:
-    p.parse(fp)
-except ParseError as perr:
-    print("perr" + str(perr))
-fp.close()
+	def next_token(self):
+		""" Returns tuple (next_token,matched-text). """
+		
+		return self.scanner.read()		
+
+	
+	def position(self):
+		""" Utility function that returns position in text in case of errors.
+		Here it simply returns the scanner position. """
+		
+		return self.scanner.position()
+	
+
+	def match(self,token):
+		""" Consumes (matches with current lookahead) an expected token.
+		Raises ParseError if anything else is found. Acquires new lookahead. """ 
+		
+		if self.la==token:
+			self.la,self.val = self.next_token()
+		else:
+			raise ParseError("found {} instead of {}".format(self.la,token))
+	
+	def parse(self,fp):
+		""" Creates scanner for input file object fp and calls the parse logic code. """
+
+		# create the plex scanner for fp
+		self.create_scanner(fp)
+		
+		# call parsing logic
+		self.stmt_list()
+
+	def stmt_list(self):
+		#print('stmt_list - ', self.la)
+		st_lst = ['ID', 'PRINT']
+		if self.la in st_lst:
+			self.stmt()
+			self.stmt_list()
+		elif self.la is None: 
+			return # gia na mporei na anagnorize >=0 entoles
+		else:
+			self.debug_message('stmt_list')
+			raise ParseError('waiting for identifier or print your input {}', self.la)
+
+	def stmt(self):
+		#print('stmt - ', self.la)
+		if self.la == 'ID':
+			self.match('ID')
+			self.match('=')
+			self.expr()
+		elif self.la == 'PRINT':
+			self.match('PRINT')
+			self.expr()
+		else:
+			self.debug_message('stmt')
+			raise ParseError('Expected identifier print', self.la)
+
+	def expr(self):
+		expr_lst = ['(','ID','TRUE', 'FALSE', 'not']
+		if self.la in expr_lst:
+			self.term()
+			self.term_tail()
+		else:
+			self.debug_message('expr')
+			expected_str = join_list(expr_lst)
+			raise ParseError('Expected : {} '.format(expected_str))
+
+
+	def term_tail(self):
+
+		tt_lst = ['or',')', 'ID', 'PRINT', None]
+
+		if self.la == 'or':
+			self.match('or')
+			self.term()
+			self.term_tail()
+		elif self.la in tt_lst[1:]:
+			return
+		else:
+			self.debug_message('term_tail')
+			expected_str = join_list(tt_lst)
+			raise ParseError('Expected : {}'.format(expected_str))		 	
+	
+	def term(self):
+		t_lst = ['FALSE', 'TRUE', '(', 'not', 'ID']
+		if self.la in t_lst:
+			self.negative_factor()
+			self.factor_tail()
+			return
+		else:
+			self.debug_message('term')
+			expected_str = join_list(t_lst)
+			raise ParseError('Expected : {}'.format(expected_str))
+
+	def factor_tail(self):
+		#print('factor_tail - ', self.la)
+		ft_lst = ['and', 'or', ')', 'PRINT', 'ID', None]
+		if self.la == 'and':
+			op = self.match('and')
+			f = self.negative_factor()
+			ft = self.factor_tail()
+
+			if ft is None:
+				return op, f
+			if ft[0] == 'and':
+				return op, f and ft[1]
+
+		elif self.la in ft_lst[1:]:
+			return
+		else:
+			self.debug_message('factor_tail')
+			expected_str = join_list(ft_lst)
+			raise ParseError("Expected : {}".format(expected_str))
+
+	def negative_factor(self):
+		if self.la == 'not':
+			self.match('not')
+
+		return self.factor()
+
+	def factor(self):
+		#print('factor - ', self.la)
+		nf_lst = ['not', '(', 'ID', 'TRUE', 'FALSE']
+		if self.la == '(':
+			#print('BASE CASE MATCHED SOMETHING ...')
+			self.match('(')
+			self.expr()
+			self.match(')')
+		elif self.la in nf_lst[2: ]:
+			#print('BASE CASE FOUND MATCHED SOMETHING ...')
+			self.match(self.la)
+		else:
+			self.debug_message('factor')
+			expected_str = join_list(nf_lst[2:])
+			raise ParseError('Expected : {}'.format(expected_str))
+
+# create the parser object
+parser = MyParser(debug = False)
+
+# open file for parsing
+with open("data.txt","r") as fp:
+
+	# parse file
+	try:
+		parser.parse(fp)
+	except plex.errors.PlexError:
+		_,lineno,charno = parser.position()	
+		print("Scanner Error: at line {} char {}".format(lineno,charno+1))
+	except ParseError as perr:
+		_,lineno,charno = parser.position()	
+		print("Parser Error: {} at line {} char {}".format(perr,lineno,charno+1))
+	except RunError as rerror:
+		_,lineno,charno = parser.position()	
+		print("Parser Error: {} at line {} char {}".format(rerr,lineno,charno+1))
--- runner.py
+++ runner.py
@@ -1,208 +1,302 @@
+#!/usr/bin/env python3
+# -*- coding: utf_8 -*-
+
 import plex
 
-
 class ParseError(Exception):
-    pass
-
-
+	""" A user defined exception class, to describe parse errors. """
+	pass
 class RunError(Exception):
-    pass
+	pass
 
-def boolean_value(str1, str2, option):
-    if option == "and":
-        if str1 == "False" or str2 == "False":
-            return "False"
-        else:
-            return "True"
-    elif option == "or":
-        if str1 == "False" and str2 == "False":
-            return "False"
-        else:
-            return "True"
-    else:
-        return "WRONG-INPUT"
+'''
+	conversts our list to boolean
+'''
+def conv_strtobool(str1):
+	if str1 is None:
+		raise RunError('str is none')
+	str_upper = str1.upper()
+	if str_upper == 'TRUE':
+		return True
+	elif str_upper == 'FALSE':
+		return False
+	else:
+		raise RunError('true or false is only accepted you entered:  {}', str)
 
-def revbool_value(str1, rev):
-    if rev:
-        if str1 == "True":
-            return "False"
-        elif str1 == "False":
-            return "True"
-        else:
-            return str1
-    else:
-        return str1
+def join_list(lst):
+	return ' '.join(str(e) for e in lst)
 
-class MyParser():
+def rev_boolif(b, rev):
+	if rev:
+		return not b
+	else:
+		return b			
 
-    def __init__(self):
-        self.st = {}
 
-    def create_scanner(self, fp):
-        letter = plex.Range("azAZ")
-        digit = plex.Range("09")
+# def boolean_ops(b1, b2, mode):
+# 	mode_l = mode.lower()
 
-        letter = plex.Range("azAZ")
-        digit = plex.Range("09")
+# 	if mode_l == 'or':
+# 		return b1 or b2
+# 	elif mode_l == 'and':
+# 		return b1 and b2
+# 	else:
+# 		raise RunError('and or false is only supported !')
 
-        ops = plex.Str("not", "and", "or")
-        kprint = plex.Str("print")
-        true = plex.NoCase(plex.Str("true", "t", "1"))
-        false = plex.NoCase(plex.Str("false", "f", "0"))
-        identifier = letter + plex.Rep(letter | digit)
-        assign = plex.Str("=")
-        space = plex.Rep1(plex.Any(" \n\t"))
-        par = plex.Any("()")
+class MyParser:
+	""" A class encapsulating all parsing functionality
+	for a particular grammar. """
 
-        lexicon = plex.Lexicon([
-            (kprint, plex.TEXT),
-            (ops, plex.TEXT),
-            (true, "True"),
-            (false, "False"),
-            (identifier, "IDENTIFIER"),
+	def __init__(self, debug = True):
+		self.debug = debug
+		self.st = {} # our variables names are stores here
+
+	def debug_message(self, caller):
+		if self.debug:
+			print('--------------------------------')
+			print('DEBUG MESSAGE : ')
+			print('CALLER : ', caller)
+			print('LOOK_AHEAD :  ', self.la)
+	
+	def create_scanner(self,fp):
+		""" Creates a plex scanner for a particular grammar 
+		to operate on file object fp. """
+
+		letter = plex.Range("azAZ")
+		digit = plex.Range("09")
+
+		ops = plex.Str("not", "and", "or")
+		kprint = plex.Str("print")
+		true = plex.NoCase(plex.Str("true", "t", "1"))
+		false = plex.NoCase(plex.Str("false", "f", "0"))
+		identifier = letter + plex.Rep(letter | digit)
+		assign = plex.Str("=")
+		space = plex.Rep1(plex.Any(" \n\t"))
+		par = plex.Any("()")
+
+		lexicon = plex.Lexicon([
+			(kprint, "PRINT"),
+			(ops, plex.TEXT),
+            (true, "TRUE"),
+            (false, "FALSE"),
+            (identifier, "ID"),
             (assign, "="),
             (par, plex.TEXT),
             (space, plex.IGNORE)
         ])
         
-        self.scanner = plex.Scanner(lexicon, fp)
-        self.la, self.val = self.next_token()
+		self.scanner = plex.Scanner(lexicon, fp)
+		self.la, self.val = self.next_token()
 
-    def next_token(self):
-        return self.scanner.read()
+	def boolean_ops(self,t, tt):
+		#print(' boolean_ops -  ', t)
+		left = t # to aristero meros you operator
+		if tt is None:
+			#print('(1)boolean_ops returning ', left)
+			return left
 
-    def match(self, token):
-        if self.la == token:
-            self.la, self.val = self.next_token()
-        else:
-            raise ParseError("Expected: ", self.la)
+		op = tt[0]
+		right = tt[1] # to deksi meros tou operator
 
-    def parse(self, fp):
-        self.create_scanner(fp)
-        self.stmt_list()
-
-    def stmt_list(self):
-        if self.la == "IDENTIFIER" or self.la == "print": 
-            self.stmt()
-            self.stmt_list()
-        elif self.la is None:
-            return
-        else:
-            raise ParseError("Expected: identifier or print")
-
-    def stmt(self):
-        if self.la == "IDENTIFIER":
-            varname = self.val
-            self.match("IDENTIFIER")
-            self.match("=")
-            self.st[varname] = self.expr()
-        elif self.la == "print":
-            self.match("print")
-            print(self.expr())
-        else:
-            raise ParseError("Expected: identifier or print")
-
-    def expr(self):
-        if self.la in  self.la == "(" or self.la == "IDENTIFIER" or self.la == "not" or self.la == "True" or self.la == "False":
-            t = self.term()
-            tt = self.term_tail()
-            if tt is None:
-                return t
-            if tt[0] == "or":
-                return boolean_value(t, tt[1], 'or')
-        else:
-            raise ParseError("Expected: '(' or identifier or not or boolean value")
-
-    def term_tail(self):
-        if self.la == "or":
-            #print("term-tail", self.la)
-            op = self.orop()
-            t = self.term()
-            tt = self.term_tail()
-            if tt is None:
-                #print("tt is none term found is", t)
-                return op, t
-            if tt[0] == "or":
-               # print("boolean_value of t and tt[1] is",t, tt[1], boolean_value(t, tt[1],'or'))
-                return op, boolean_value(t, tt[1], "or")
-        elif self.la == "IDENTIFIER" or self.la == "print" or self.la == ")" or self.la is None: 
-            return
-        else:
-             raise ParseError('Expected or print or None or identifier ')
-
-    def term(self):
-        if self.la == "(" or self.la == "not" or self.la == "IDENTIFIER" or self.la == "True" or self.la == "False": 
-            #f = self.factor()
-            f = self.negative_factor()
-            ft = self.factor_tail()
-            if ft is None:
-                return f
-            if ft[0] == "and":
-                return boolean_value(f, ft[1], "and")
-        else:
-            raise ParseError("Expected: '(' or identifier or boolean value")
-
-    def factor_tail(self):
-        if self.la == "and":
-            op = self.andop()
-            #f = self.factor()
-            f = self.negative_factor()
-            ft = self.factor_tail()
-            if ft is None:
-                return op, f
-            if ft[0] == "and":
-                return op, boolean_value(f, ft[1], "and")
-        elif self.la == "or" or self.la == "print" or self.la =="IDENTIFIER" or self.la == ")" or self.la is None:   
-            return
-        else:
-            raise ParseError("Expected: 'and'")
-
-    def negative_factor(self):
-        negative = False
-        if self.la == 'not':
-            #print("ITS A negative")
-            negative = True
-            self.match("not")
-        return revbool_value(self.factor(), negative)
-
-    def factor(self):
-        if self.la == '(':
-            self.match('(')
-            e = self.expr()
-            self.match(')')
-            return e
-        elif self.la == "IDENTIFIER":
-            varname = self.val
-            self.match(self.la)
-            if varname in self.st:
-                return self.st[varname]
-            raise RunError("Unitialized variable: ", varname)
-        elif self.la == "True" or self.la == "False":
-            token = self.la
-            self.match(token)
-            return token
-        else:
-            raise ParseError("Expected: notop id,  notop(expr), notop values, (expr), id, notop boolean")
-
-    def orop(self):
-        if self.la == "or":
-            self.match("or")
-            return "or"
-        else:
-            raise ParseError("Expected: 'or'")
-
-    def andop(self):
-        if self.la == "and":
-            self.match("and")
-            return "and"
-        else:
-            raise ParseError("Expected: 'and'")
+		if op == 'or':
+			#print('(2)boolean_ops returning - ', left, right)
+			return left or right
+		elif op == 'and':
+			#print('(1)boolean_ops returning - ', left,right)
+			return left and right
+		else:
+			raise RunError('supported operators are: and or ')
 
 
-p = MyParser()
-fp = open("data.txt", "r")
-try:
-    p.parse(fp)
-except ParseError as perr:
-    print("perr" + str(perr))
-fp.close()
+
+	def next_token(self):
+		""" Returns tuple (next_token,matched-text). """
+		
+		return self.scanner.read()		
+
+	
+	def position(self):
+		""" Utility function that returns position in text in case of errors.
+		Here it simply returns the scanner position. """
+		
+		return self.scanner.position()
+	
+
+	def match(self,token):
+		""" Consumes (matches with current lookahead) an expected token.
+		Raises ParseError if anything else is found. Acquires new lookahead. """ 
+		
+		if self.la==token:
+			self.la,self.val = self.next_token()
+		else:
+			raise ParseError("found {} instead of {}".format(self.la,token))
+	
+	def parse(self,fp):
+		""" Creates scanner for input file object fp and calls the parse logic code. """
+
+		# create the plex scanner for fp
+		self.create_scanner(fp)
+		
+		# call parsing logic
+		self.stmt_list()
+
+	def stmt_list(self):
+		#print('stmt_list - ', self.la)
+		st_lst = ['ID', 'PRINT']
+		if self.la in st_lst:
+			self.stmt()
+			self.stmt_list()
+		elif self.la is None: 
+			return # gia na mporei na anagnorize >=0 entoles
+		else:
+			self.debug_message('stmt_list')
+			raise ParseError('waiting for identifier or print your input {}', self.la)
+
+	def stmt(self):
+		#print('stmt - ', self.la)
+		if self.la == 'ID':
+			vname = self.val
+			self.match('ID')
+			self.match('=')
+			self.st[vname] = self.expr() # anathesi 
+		elif self.la == 'PRINT':
+			self.match('PRINT')
+			print(self.expr()) # ektuposi timis apotelesmaton
+		else:
+			self.debug_message('stmt')
+			raise ParseError('waiting for identifier or print', self.la)
+
+
+	def expr(self):
+		#print('expr ', self.la)
+		expr_lst = ['(','ID','TRUE', 'FALSE', 'not']
+		if self.la in expr_lst:
+			t = self.term()
+			tt = self.term_tail()
+			#print('expr {} - {}'.format(t, tt))
+			return self.boolean_ops(t, tt)
+		else:
+			self.debug_message('expr')
+			expected_str = join_list(expr_lst)
+			raise ParseError('Expected : {} '.format(expected_str))
+
+
+	def term_tail(self):
+		#print('term_tail - ', self.la)
+		tt_lst = ['or',')', 'ID', 'PRINT', None]
+
+		if self.la == 'or':
+			self.match('or')
+			t = self.term()
+			tt = self.term_tail()
+			return 'or',self.boolean_ops(t, tt)
+		elif self.la in tt_lst[1:]:
+			return
+		else:
+			self.debug_message('term_tail')
+			#raise ParseError('Expected or ')
+			expected_str = join_list(tt_lst)
+			raise ParseError('Expected : {} '.format(expected_str))		 	
+	
+	def term(self):
+		#print('term - ', self.la)
+		t_lst = ['FALSE', 'TRUE', '(', 'not', 'ID']
+		if self.la in t_lst:
+			f = self.negative_factor()
+			ft = self.factor_tail()
+			#print('term - f ', f)
+			#print('term - ft', ft)
+			return self.boolean_ops(f, ft)
+		else:
+			self.debug_message('term')
+			expected_str = join_list(t_lst)
+			raise ParseError('Expected : {}'.format(expected_str))
+
+	def factor_tail(self):
+		#print('factor_tail - ', self.la)
+		ft_lst = ['and', 'or', ')', 'PRINT', 'ID', None]
+		#print(" begin factor_tail - ", self.la )
+		if self.la == 'and':
+			self.match('and')
+			op = 'and'
+			f = self.negative_factor()
+			ft = self.factor_tail()
+			return  op, self.boolean_ops(f, ft)
+		elif self.la in ft_lst[1:]:
+			return
+		else:
+			self.debug_message('factor_tail')
+			expected_str = join_list(ft_lst)
+			raise ParseError("Expected : {}".format(expected_str))
+
+	def negative_factor(self):
+		#print('negative_factor - ', self.la)
+		negative = False
+		if self.la == 'not':
+			self.match('not')
+			negative = True
+		return rev_boolif(self.factor(), negative)
+
+	def get_idvalue(self, vname):
+		#print('getting identifiers value : ')
+		if vname in self.st:
+			return self.st[vname]
+		else:
+			raise RunError('Variable {} is not initialised !'.format(vname))
+
+	def or_op(self):
+		if self.la == 'or':
+			self.match('or')
+			return 'or'
+		else:
+			raise ParseError('Expected or')
+
+	def and_op(self):
+		if self.la == 'and':
+			self.match('and')
+			return 'and'
+		else:
+			raise ParseError('Expected and ')
+
+	def factor(self):
+		#print('factor - ', self.la)
+		nf_lst = ['not', '(', 'ID', 'TRUE', 'FALSE']
+		if self.la == '(':
+			#print('BASE CASE MATCHED SOMETHING ...')
+			self.match('(')
+			e = self.expr()
+			self.match(')')
+			return e
+		elif self.la in ['TRUE', 'FALSE']:
+			#print('converting to boolean - ', self.la)
+			boolean = conv_strtobool(self.la)
+			self.match(self.la)
+			return boolean 		
+		elif self.la == 'ID':
+			vname = self.val
+			#print("VARIABLE NAME IS: ", self.val)
+			self.match(self.la)
+			return self.get_idvalue(vname)
+		else:
+			self.debug_message('factor')
+			expected_str = join_list(nf_lst[2:])
+			raise ParseError('Expected : {}'.format(expected_str))
+
+# create the parser object
+parser = MyParser(debug = False)
+
+# open file for parsing
+with open("data.txt","r") as fp:
+
+	# parse file
+	try:
+		parser.parse(fp)
+	except plex.errors.PlexError:
+		_,lineno,charno = parser.position()	
+		print("Scanner Error: at line {} char {}".format(lineno,charno+1))
+	except ParseError as perr:
+		_,lineno,charno = parser.position()	
+		print("Parser Error: {} at line {} char {}".format(perr,lineno,charno+1))
+	except RunError as rerror:
+		_,lineno,charno = parser.position()	
+		print("Parser Error: {} at line {} char {}".format(rerror,lineno,charno+1))
